d06ee83783961a20ca6f85ffa4fb74ab7a0547b9
Darry <darry0703@gmail.com>
2021-03-16 20:11:03 +0800

    Add files via upload

--- keyer_Effects.py
+++ keyer_Effects.py
@@ -12,22 +12,69 @@ cv2.imwrite("/home/pi/stoneScanner/data/pic/picture.png", image)
 # 刪除webcam，避免影像佔用資源
 del(webcam)
 
-img=cv2.imread('/home/pi/stoneScanner/data/pic/picture.png')
-## (1) Convert to gray, and threshold
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-print(gray)
+# Read image
+img = cv2.imread('/home/pi/stoneScanner/data/pic/picture.png')
+hh, ww = img.shape[:2]
 
-#二值化，将背景150到255的像素值改为255
-th, threshed = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
-## (2) Morph-op to remove noise
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
-morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
-## (3) Find the max-area contour
-cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-cnt = sorted(cnts, key=cv2.contourArea)[-1]
-## (4) Crop and save it
-x,y,w,h = cv2.boundingRect(cnt)
+# threshold on white
+# Define lower and uppper limits
+lower = np.array([200, 200, 200])
+upper = np.array([255, 255, 255])
 
-#取反色，删除背景的影响。
-dst = 255-img[y:y+h, x:x+w]
-cv2.imwrite("/home/pi/stoneScanner/data/pic/001.png", dst)
+# Create mask to only select black
+thresh = cv2.inRange(img, lower, upper)
+
+# apply morphology
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+# get contours
+contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+
+# draw white contours on black background as mask
+mask = np.zeros((hh,ww), dtype=np.uint8)
+for cntr in contours:
+    cv2.drawContours(mask, [cntr], 0, (255,255,255), -1)
+
+# get convex hull
+points = np.column_stack(np.where(thresh.transpose() > 0))
+hullpts = cv2.convexHull(points)
+((centx,centy), (width,height), angle) = cv2.fitEllipse(hullpts)
+print("center x,y:",centx,centy)
+print("diameters:",width,height)
+print("orientation angle:",angle)
+
+# draw convex hull on image
+hull = img.copy()
+cv2.polylines(hull, [hullpts], True, (0,0,255), 1)
+
+# create new circle mask from ellipse 
+circle = np.zeros((hh,ww), dtype=np.uint8)
+cx = int(centx)
+cy = int(centy)
+radius = (width+height)/8
+cv2.circle(circle, (cx,cy), int(radius), 255, -1)
+
+# erode circle a bit to avoid a white ring
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,6))
+circle = cv2.morphologyEx(circle, cv2.MORPH_ERODE, kernel)
+
+# combine inverted morph and circle
+mask2 = cv2.bitwise_and(255-morph, 255-morph, mask=circle)
+
+# apply mask to image
+result = cv2.bitwise_and(img, img, mask=mask2)
+
+result = cv2.blur(result, (5,5))
+
+# save results
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_thresh2.jpg', thresh)
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_morph2.jpg', morph)
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_mask2.jpg', mask)
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_hull2.jpg', hull)
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_circle.jpg', circle)
+cv2.imwrite('/home/pi/stoneScanner/data/pic/pills_result2.png', result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
