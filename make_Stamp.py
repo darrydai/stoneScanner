a68bb44c371a8cfe9642b61af19537112f4e0062
darrydai <darry0703@gmail.com>
2021-04-06 02:52:36 +0800

    update ML model,add cap err function

--- make_Stamp.py
+++ make_Stamp.py
@@ -4,70 +4,96 @@ import numpy
 import numpy as np
 import random
 import subprocess
+import logging
 
 config = configparser.ConfigParser()
 config.read('/home/pi/stoneScanner/stone_scanner.ini')
 
 def make_Stamp(stoneNum,stampNum,stone_id):
-    
-    bg_Path='/home/pi/stoneScanner/data/pic/stamp/background/Text_1_01.png'
-    if stone_id == 4:
-        bg_Path=config['stamp']['stamp_BG']+'/Text_4_01.png'
-    if stone_id == 6:
-        bg_Path=config['stamp']['stamp_BG']+'/Text_6_01.png'
-    if stone_id == 13:
-        bg_Path=config['stamp']['stamp_BG']+'/Text_13_01.png'
-    #path
-    #bg_Path=config['stamp']['stamp_BG']+'/Text_'+str(random.randint(1,14))+'_01.png'
-    stoneImg_path=config['stamp']['stone_BG_removed']
-    save_path=config['stamp']['stamp_save']+'/stamp_'+str(stampNum)+'.png'
+    try:
+        if stone_id == 'smooth':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_1_01.png'
+        if stone_id == 'wave':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_2_01.png'
+        if stone_id == 'ground':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_3_01.png'
+        if stone_id == 'sun':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_4_01.png'
+        if stone_id == 'rose':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_6_01.png'
+        if stone_id == 'snake':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_7_01.png'
+        if stone_id == 'mwaji':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_8_01.png'
+        if stone_id == 'unknow':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_10_01.png'
+        if stone_id == 'moon':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_11_01.png'
+        if stone_id == 'snow':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_12_01.png'
+        if stone_id == 'mountain':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_13_01.png'
+        if stone_id == 'skinny':
+            bg_Path=config['stamp']['stamp_BG']+'/Text_14_01.png'
+        # if stone_id == 'mountain':
+        #     bg_Path=config['stamp']['stamp_BG']+'/Text_13_01.png'
 
-    #Image processing parameter
-    sharpness = float(config['stamp']['sharpness'])
-    contrast = float(config['stamp']['contrast'])
-    color = float(config['stamp']['color'])
+        #path
+        #bg_Path=config['stamp']['stamp_BG']+'/Text_'+str(random.randint(1,14))+'_01.png'
+        stoneImg_path=config['stamp']['stone_BG_removed']
+        save_path=config['stamp']['stamp_save']+'/stamp_'+str(stampNum)+'.png'
 
-    #stamp background black area
-    blackArea = [498,450]
+        #Image processing parameter
+        sharpness = float(config['stamp']['sharpness'])
+        contrast = float(config['stamp']['contrast'])
+        color = float(config['stamp']['color'])
 
-    #load background
-    bg_Img=Image.open(bg_Path)
-    bg_Img=bg_Img.convert('RGBA')
+        #stamp background black area
+        blackArea = [498,450]
 
-    #load stone image
-    stoneImg=Image.open(stoneImg_path+'stone_'+str(stoneNum)+'.png')
-    stoneImg=stoneImg.convert('RGBA')
+        #load background
+        bg_Img=Image.open(bg_Path)
+        bg_Img=bg_Img.convert('RGBA')
 
-    #Enhance stone image sharpness
-    enh_sha = ImageEnhance.Sharpness(stoneImg)
-    stoneImg = enh_sha.enhance(sharpness)
+        #load stone image
+        stoneImg=Image.open(stoneImg_path+'stone_'+str(stoneNum)+'.png')
+        stoneImg=stoneImg.convert('RGBA')
 
-    #Enhance stone image contrast
-    enh_con = ImageEnhance.Contrast(stoneImg)
-    stoneImg = enh_con.enhance(contrast)
+        #Enhance stone image sharpness
+        enh_sha = ImageEnhance.Sharpness(stoneImg)
+        stoneImg = enh_sha.enhance(sharpness)
 
-    #Enhance stone image color
-    enh_col = ImageEnhance.Color(stoneImg)
-    stoneImg = enh_col.enhance(color)
+        #Enhance stone image contrast
+        enh_con = ImageEnhance.Contrast(stoneImg)
+        stoneImg = enh_con.enhance(contrast)
 
-    #resize stone image
-    stoneImg.thumbnail((295,295),Image.BILINEAR)
-    stoneW,stoneH=stoneImg.size
+        #Enhance stone image color
+        enh_col = ImageEnhance.Color(stoneImg)
+        stoneImg = enh_col.enhance(color)
 
-    stoneCopy=stoneImg
+        #resize stone image
+        stoneImg.thumbnail((295,295),Image.BILINEAR)
+        stoneW,stoneH=stoneImg.size
 
-    #convert image to gary then to binary
-    stoneCopy=stoneCopy.convert('L')
-    stoneCopy=stoneCopy.convert('1')
+        stoneCopy=stoneImg
 
-    #shift the stone image position
-    bg_Img.paste(stoneCopy,(int((blackArea[0]-stoneW)/2),int((blackArea[1]-stoneH)/2)),stoneImg)
-    bg_Img.save(save_path,dpi=(600,300))
+        #convert image to gary then to binary
+        stoneCopy=stoneCopy.convert('L')
+        stoneCopy=stoneCopy.convert('1')
 
-    subprocess.run(["sudo","brother_ql","-b","linux_kernel","-p","/dev/usb/lp0","-m","QL-800","print","-l","62","--red",save_path],stdout=subprocess.PIPE)
+        #shift the stone image position
+        bg_Img.paste(stoneCopy,(int((blackArea[0]-stoneW)/2),int((blackArea[1]-stoneH)/2)),stoneImg)
+        bg_Img.save(save_path,dpi=(600,300))
+
+        subprocess.run(["sudo","brother_ql","-b","linux_kernel","-p","/dev/usb/lp0","-m","QL-800","print","-l","62","--red",save_path],stdout=subprocess.PIPE)
+    except Exception as e:
+        print("發生錯誤：",e)
 
 def main():
-    make_Stamp(0,0)
+    try:
+        make_Stamp(0,0)
+    except Exception as e:
+        print("發生錯誤：",e)
 
 if __name__ == '__main__':
     main()
